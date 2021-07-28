5a5dd247c7c1e79e6a7358ebf2cff782df4c93d0
Sato Ryoka <satoryoka0920@gmail.com>
2021-07-29 08:39:44 +0900

    shuffle

--- deck.go
+++ deck.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -15,7 +18,7 @@ func newDeck() deck {
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, suit+" of "+value)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 	// fmt.Println(cards)
@@ -40,4 +43,27 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+
 }
--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	fmt.Println(cards.toString())
-	cards.saveToFile("my_cards")
-
-	// for _, card := range cards {
-	// 	fmt.Println(card)
-	// }
-
-	//cards.print()
-	// hand, remainingCards := deal(cards, 5)
-	// hand.print()
-	// remainingCards.print()
-
+	cards.shuffle()
+	cards.print()
 }
